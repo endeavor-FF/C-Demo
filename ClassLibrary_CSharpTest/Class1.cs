b4877a72df1ec7d97ffe29001a3e51f9332c8b68
Wang.F <1721063633@qq.com>
2022-09-07 22:45:13 +0800

    anontation update

--- ClassLibrary_CSharpTest/Class1.cs
+++ ClassLibrary_CSharpTest/Class1.cs
@@ -4,13 +4,12 @@ using System.Collections.Generic;
 //C#类库项目没有启动项，需要在类库项目中引入一个可启动项。并将其设为启动项
 namespace ClassLibrary_CSharpTest
 {
-    
     public class Class1:Student//继承  基类
     {
-        List<Student> slst;
+        List<Student> slst;//创建一个对象
         public Class1() 
         {
-            slst = new List<Student>();
+            slst = new List<Student>();//对象实例化
             this.sGender = "male";
             this.sName = "Mike";
             this.sHobby = "网球";
@@ -19,13 +18,19 @@ namespace ClassLibrary_CSharpTest
             std.sHobby = this.sHobby;
             std.sName = this.sName;
             slst.Add(std);
-           // string sJSON = Newtonsoft.Json.JsonConvert.SerializeObject(slst);
-            Console.WriteLine(slst);
         }   
         public string CallPid()//public封装使得该方法在别的类中可以调用
         {
             string sRetValue;
             string sJSON = Newtonsoft.Json.JsonConvert.SerializeObject(this.slst);
+            /*[{
+            \"sName\":\"Mike\",
+            \"sGender\":\"male\",
+            \"sHobby\":\"网球\",
+            \"debug1\":0,
+            \"debug2\":0,
+            \"debug3\":0
+            }]*/
             var data = new MessageTag()
             {
                 sRet01 = "1",
